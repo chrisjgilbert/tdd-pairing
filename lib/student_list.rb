107261567db143c0b6a878cc747f8872f8097df8
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-01 18:18:10 +0000

    tests implemented and passed for all initial user stories

--- lib/student_list.rb
+++ lib/student_list.rb
@@ -1,7 +1,19 @@
 class StudentList
-
+  attr_reader :students
   def initialize
     @students = []
+  end
+
+  def add(name)
+    @students << name
+  end
+
+  def count_students
+    @students.count
+  end
+
+  def clear_students
+    @students.clear
   end
 
 end
--- spec/student_list_spec.rb
+++ spec/student_list_spec.rb
@@ -4,21 +4,37 @@ describe StudentList do
 
   let(:studentlist) { StudentList.new }
 
+  describe "#add_student" do
+    it "can add a student's name to the student list" do
+      studentlist.add("Chris")
+      expect(studentlist.students).to eq ["Chris"]
+    end
 
-  it "can add a student's name to the student list" do
+    it "can add multiple names to the student list" do
+      studentlist.add("Chris")
+      studentlist.add("Darci")
+      studentlist.add("Makers")
+      expect(studentlist.students).to eq ["Chris", "Darci", "Makers"]
+    end
 
   end
 
-  it "can add multiple names to the student list" do
-
+  describe "#count_students" do
+    it "can count how many students have been created" do
+      studentlist.add("Chris")
+      studentlist.add("Darci")
+      studentlist.add("Makers")
+      expect(studentlist.count_students).to eq 3
+    end
   end
 
-  it "can count how many students have been created" do
-
-  end
-
-  it "can clear the student list" do
-
+  describe "#clear_students" do
+    it "can clear the student list" do
+      studentlist.add("Chris")
+      studentlist.add("Darci")
+      studentlist.add("Makers")
+      expect(studentlist.clear_students).to eq []
+    end
   end
 
 end
